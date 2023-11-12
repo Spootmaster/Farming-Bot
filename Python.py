dca77e42984739d1a5482a28b63c461441fe5ccc
Spootmaster <148012563+Spootmaster@users.noreply.github.com>
2023-11-12 15:59:53 -0600

    Main File, First Commit
    Basic Code of with pynput, pyautogui, time.

---
+++ Python.py
@@ -0,0 +1,37 @@
+from pynput import keyboard
+import pyautogui
+import time
+
+#List to store recorded keystrokes
+keystrokes = []
+
+#Function to record key presses
+def on_key_press(key):
+    try:
+        keystrokes.append(key.char)
+    except AttributeError:
+        #Handle special keys
+        keystrokes.append(str(key))
+
+#Keystrokes Recorder
+with keyboard.Listener(on_press=on_key_press) as listener:
+    listener.join()
+
+#Print the recorded keystrokes
+print("Recorded Keystrokes:", keystrokes)
+
+#Replay Keystrokes
+def play_back_keystrokes(keystrokes):
+    with keyboard.Controller() as controller:
+        for key in keystrokes:
+            controller.press(key)
+            controller.release(key)
+            time.sleep(0.1)
+
+# Play back the recorded keystrokes
+play_back_keystrokes(keystrokes)
+
+#interact with the screen
+pyautogui.moveTo(500, 500)
+pyautogui.click()
+pyautogui.typewrite("Test Test")
